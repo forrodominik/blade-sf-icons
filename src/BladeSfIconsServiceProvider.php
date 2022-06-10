e70812964b8a7a07cc84242ffb593e84024662ea
Forró Dominik <forro.dominik@fakultativ.hu>
2022-06-10 14:31:26 +0200

    Icon errors fixed

--- src/BladeSfIconsServiceProvider.php
+++ src/BladeSfIconsServiceProvider.php
@@ -17,24 +17,24 @@ final class BladeSfIconsServiceProvider extends ServiceProvider
         $this->callAfterResolving(Factory::class, function (Factory $factory, Container $container) {
             $config = $container->make('config')->get('blade-sf-icons', []);
 
-            $factory->add('sf-icon', array_merge(['path' => __DIR__.'/../resources/svg'], $config));
+            $factory->add('sf-icon', array_merge(['path' => __DIR__ . '/../resources/svg'], $config));
         });
     }
 
     private function registerConfig(): void
     {
-        $this->mergeConfigFrom(__DIR__.'/../config/blade-sf-icons.php', 'blade-sf-icons');
+        $this->mergeConfigFrom(__DIR__ . '/../config/blade-sf-icons.php', 'blade-sf-icons');
     }
 
     public function boot(): void
     {
         if ($this->app->runningInConsole()) {
             $this->publishes([
-                __DIR__.'/../resources/svg' => public_path('vendor/blade-sf-icons'),
-            ], 'blade-heroicons');
+                __DIR__ . '/../resources/svg' => public_path('vendor/blade-sf-icons'),
+            ], 'blade-sf-icons');
 
             $this->publishes([
-                __DIR__.'/../config/blade-sf-icons.php' => $this->app->configPath('blade-sf-icons.php'),
+                __DIR__ . '/../config/blade-sf-icons.php' => $this->app->configPath('blade-sf-icons.php'),
             ], 'blade-sf-icons-config');
         }
     }
