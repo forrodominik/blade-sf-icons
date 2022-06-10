e195bbe1a3ccd36a1239dc659dc5587091ad5783
Forró Dominik <forro.dominik@fakultativ.hu>
2022-06-10 13:02:43 +0200

    Try icons in the trial pack

---
+++ src/BladeSfIconsServiceProvider.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace BladeUI\SfIcons;
+
+use BladeUI\Icons\Factory;
+use Illuminate\Contracts\Container\Container;
+use Illuminate\Support\ServiceProvider;
+
+final class BladeSfIconsServiceProvider extends ServiceProvider
+{
+    public function register(): void
+    {
+        $this->registerConfig();
+
+        $this->callAfterResolving(Factory::class, function (Factory $factory, Container $container) {
+            $config = $container->make('config')->get('blade-sf-icons', []);
+
+            $factory->add('sf-icons', array_merge(['path' => __DIR__.'/../resources/svg'], $config));
+        });
+    }
+
+    private function registerConfig(): void
+    {
+        $this->mergeConfigFrom(__DIR__.'/../config/blade-sf-icons.php', 'blade-sf-icons');
+    }
+
+    public function boot(): void
+    {
+        if ($this->app->runningInConsole()) {
+            $this->publishes([
+                __DIR__.'/../resources/svg' => public_path('vendor/blade-sf-icons'),
+            ], 'blade-heroicons');
+
+            $this->publishes([
+                __DIR__.'/../config/blade-sf-icons.php' => $this->app->configPath('blade-sf-icons.php'),
+            ], 'blade-sf-icons-config');
+        }
+    }
+}
